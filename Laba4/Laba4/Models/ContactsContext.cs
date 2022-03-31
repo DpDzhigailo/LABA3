fdcaf5118b840a8549640341eceeca3e105feb67
429283-21 <429283-21>
2022-03-31 15:24:02 +0300

    laboratornay3

---
+++ Laba4/Laba4/Models/ContactsContext.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Laba4.Models
+{
+    public class ContactsContext : DbContext
+    {
+        public DbSet<Contact> Contacts { get; set; }
+        public ContactsContext()
+        {
+            Database.EnsureCreated();
+        }
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer("Server=ADCLG1;Database=Lab44;Trusted_Connection=True;");
+        }
+    }
+}
